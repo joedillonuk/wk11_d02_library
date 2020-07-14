2fc716f7d1eddedfc56d97136f5c658007d5899a
joedillonuk <joe.dillon.uk@gmail.com>
2020-07-14 16:26:58 +0100

    Updated with library class. MVP methods completed and passing tests.

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,36 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> stock;
+    private int maxStock;
+
+    public Library(){
+        this.stock = new ArrayList<Book>();
+        this.maxStock = 5;
+
+    }
+
+    public int stockCount(){
+        return stock.size();
+    }
+
+    public void addBook(Book book){
+        stock.add(book);
+    }
+
+    public ArrayList<Book> getAllStock() {
+        return stock;
+    }
+
+    public int getMaxStock() {
+        return maxStock;
+    }
+
+    public boolean hasRoomInStock(){
+        if(stockCount() >= getMaxStock()){
+            return false;
+        }
+        return true;
+    }
 }
---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,47 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LibraryTest {
+
+    Library library;
+    Book book1, book2, book3;
+
+    @Before
+    public void before(){
+        library = new Library();
+        book1 = new Book("Revenge of the Lawn", "Richard Brautigan", "Short Stories");
+        book2 = new Book("A Small, Good Thing", "Raymond Carver", "Short Stories");
+        book3 = new Book("Catch-22", "Joseph Heller", "Dark Comedy");
+    }
+
+    @Test
+    public void checkStockStartsEmpty(){
+        assertEquals(0, library.stockCount());
+    }
+
+    @Test
+    public void canAddBookToStock(){
+        library.addBook(book1);
+        assertEquals(1, library.stockCount());
+    }
+
+    @Test
+    public void hasRoomInStock_True(){
+        assertEquals(true, library.hasRoomInStock());
+    }
+
+    @Test
+    public void hasRoomInStock_False(){
+        library.addBook(book1);
+        library.addBook(book1);
+        library.addBook(book1);
+        library.addBook(book1);
+        library.addBook(book1);
+        library.addBook(book1);
+        assertEquals(false, library.hasRoomInStock());
+    }
+
+
+}
